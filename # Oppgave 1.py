4b506f9cc755de0f722b2bbbd6f6a5f0d05eec33
David Kodehode <davidemil.kodehode@gmail.com>
2023-01-09 09:42:30 +0100

    first commit

---
+++ # Oppgave 1.py
@@ -0,0 +1,36 @@
+# Oppgave 1:
+
+a = 10
+b = 2
+
+if a + b == 25:
+    print("true")
+else:    
+    print("false")
+
+# Oppgave 2:
+
+a = 10
+b = 2
+
+val = a % b
+
+print(val)
+
+# Oppgave 3:
+
+myVariable="false"
+mySecondVariable="0"
+
+print(myVariable + mySecondVariable)
+
+# Oppgave 4:
+
+import random
+
+numberList = [1,2,3,4,5,6,7,8,9,10]
+randomNumber = random.choice(numberList)
+if (randomNumber <=10):
+     print("random number is : " + str(randomNumber))
+
+
